d5b2d1d3ad005562d988fcdca81cbee85b7682bd
Angelo N LeDonne <ledonneangelo02@gmail.com>
2024-08-13 15:30:25 -0500

    File Init

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Trade-I_FILE C:/Users/ledon/Documents/Trade-I/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Trade-I.exe)
+set(__QT_DEPLOY_TARGET_Trade-I_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Trade-I_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "tradei_mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    TradeI_MainWindow w;
+    w.show();
+    return a.exec();
+}
